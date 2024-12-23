678cc4b383a995e43212cfd1e391b14cd689e6bc
chloelo <chloe.lo@ennowell.com>
2024-12-23 12:57:24 +0800

    feat: 🎸 init first monorepo

---
+++ packages/ui-lib/src/components/BaseButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button type="button" class="button">{{ props.label }}</button>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  label: string
+}>()
+</script>
+
+<style scoped>
+.button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+</style>
